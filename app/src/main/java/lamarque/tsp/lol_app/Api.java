de4ea288c8876cfe21765c2790abe44a331c4a88
Anthony Lamarque <anthony.lamarque@telecom-sudparis.eu>
2020-12-17 00:28:15 +0100

    APIKEY 17/12

--- app/src/main/java/lamarque/tsp/lol_app/Api.java
+++ app/src/main/java/lamarque/tsp/lol_app/Api.java
@@ -2,6 +2,6 @@ package lamarque.tsp.lol_app;
 
 public class Api {
 
-    public static String API_KEY = "RGAPI-802d2c6f-4c03-496d-99d1-3af9368d7dba";
+    public static String API_KEY = "RGAPI-3a70a589-b993-4013-9bc0-defb6d3968d5";
 
 }
